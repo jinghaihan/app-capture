7bf75ed85527a3eafbbd09668506344847ea70b3
jinghaihan <jhh19980114@gmail.com>
2026-04-30 22:41:48 +0800

    refactor: mitm addon scripts

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -4,4 +4,7 @@ export default defineConfig({
   entry: ['src/index.ts', 'src/cli.ts'],
   exports: true,
   clean: true,
+  copy: [
+    { from: 'src/core/mitm_addon.py', to: 'dist', flatten: true },
+  ],
 })
